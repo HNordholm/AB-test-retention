1c44e30d767ab19ce37bdfc1d815c9cdea7be95f
Hampus Nordholm <hampus.nordholm@live.se>
2025-07-29 14:27:33 +0200

    Adding SSMS connection and loading script

---
+++ scripts/load_data.R
@@ -0,0 +1,23 @@
+## SSMS CONNECTION AND LOADING DATA ## 
+###### DB CONNECTOR AND QUERY #######
+
+
+# Libraries -- 
+library(DBI)
+library(odbc)
+library(tidyverse)
+
+## Database connection --
+
+SSMS_connection <- dbConnect(odbc::odbc(),
+                             Driver = "SQL Server",
+                             Server = "LAPTOP-73T11QD1\\SQLEXPRESS",
+                             Database = "ab_db",
+                             Trusted_Connection = "Yes")
+
+## Loading data into tibble -- 
+
+ab_tbl  <- dbGetQuery(SSMS_connection, "SELECT userid, version, 
+                      gamerounds, retention_1 FROM ab_data")
+ab_tbl <- as_tibble(ab_tbl)
+
